c2bbdaffd626b2fc532e26b17d27b3bc9a9cd5a7
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-06-11 23:02:11 +0600

    notificationintegrated

--- helpers/enviroments.js
+++ helpers/enviroments.js
@@ -15,12 +15,22 @@ enviroments.staging = {
     envName: 'staging',
     secretKey: 'gsdfdsfgdsg',
     maxChecks: 5,
+    twilio: {
+        fromPhone: '',
+        AccountSid: 'ACbc3ba0a51f3a66af6638235543a6cb30',
+        AuthToken: '1eb57cbf52bc2906670d62ed6e9dbb8e',
+    },
 };
 enviroments.production = {
     port: 5000,
     evnName: 'production',
     secretKey: 'd5fdf5sdf2zxfc',
     maxChecks: 5,
+    twilio: {
+        fromPhone: '',
+        AccountSid: 'ACbc3ba0a51f3a66af6638235543a6cb30',
+        AuthToken: '1eb57cbf52bc2906670d62ed6e9dbb8e',
+    },
 };
 
 //determine which enviroment was passed
---
+++ helpers/notification.js
@@ -0,0 +1,48 @@
+/*
+Title: Notification library
+Description: important functions to notify users
+Author:Dibyendu 
+
+*/
+
+//dependencies
+const https = require('https');
+const { twilio } = require('./enviroments');
+const querystring = require('querystring');
+//module scuffolding
+const notification = {};
+
+//send sms to user using twilio api
+notification.sendTwilioSms = (phone, sms, callback) => {
+    //input validation
+    const userPhone =
+        typeof phone === 'string' && phone.trim().length === 11
+            ? phone.trim()
+            : false;
+    const userMsg =
+        typeof msg === 'string' &&
+        msg.trim().length > 0 &&
+        msg.trim().length < 1600
+            ? msg.trim()
+            : false;
+    if (userPhone && userMsg) {
+        //configure the request payload
+        const payload = {
+            From: twilio.fromPhone,
+            To: `+88${userPhone}`,
+            Body: userMsg,
+        };
+        //stringify the payload
+        const stringifyPayload = querystring.stringify(payload);
+        //configure the request details
+        const requestDetails = {
+            hostname: 'api.twilio.com',
+            method: 'POST',
+            path: ``,
+        };
+    } else {
+        callback('Given input invalid');
+    }
+};
+
+exports.module = notification;
