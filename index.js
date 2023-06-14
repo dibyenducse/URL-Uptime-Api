d02d38875d32c467f4e69fab621558fbcc8c1871
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-06-14 20:56:44 +0600

    worker and server initialized

--- index.js
+++ index.js
@@ -1,57 +1,23 @@
 /*
-Title: URL Uptime Monitoring Application
-Description: A RESTFul API to monitor up or down time of user defined links
+Title: Initial file
+Description: initial file to start the node server and workers
 Author: Dibyendu
 Bloombrain
 */
 
 //Dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const data = require('./lib/data');
-const enviroment = require('./helpers/enviroments');
-const { sendTwilioSms } = require('./helpers/notification');
+const server = require('./lib/server');
+const worker = require('./lib/worker');
 
 // App object- module scaffolfing
 const app = {};
 
-//For testing
-sendTwilioSms('01671379417', 'Hello World', (err) => {
-    console.log(err);
-});
-/*
-data.create(
-    'test',
-    'newText',
-    { name: 'dibbo', language: 'JavaScript' },
-    (err) => {
-        console.log(err);
-    }
-);
-*/
-/*
-data.delete('test', 'newFile', (err) => {
-    console.log(err);
-});
-*/
-
-//configuration
-// app.config = {
-//     port: 2000,
-// };
-
-// create server
-
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(enviroment.port, () => {
-        console.log(`enviroment variable is ${process.env.NODE_ENV}`);
-        console.log(`Listenting to port ${enviroment.port}`);
-    });
+app.init = () => {
+    //start the server
+    server.init();
+    //start the workers
+    worker.init();
 };
 
-//handle Req and Res
-app.handleReqRes = handleReqRes;
-
 //start the server
-app.createServer();
+app.init();
